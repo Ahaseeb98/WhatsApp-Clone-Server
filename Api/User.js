6c728e6107e24824c0486e8c7cf48a2eefafe3e8
Ahaseeb98 <ahaseeb12251998@gmail.com>
2020-04-13 19:02:09 +0500

    new api added

--- Api/User.js
+++ Api/User.js
@@ -10,7 +10,7 @@ router.post("/add_user", (req, res) => {
     const file = req.files["images[]"];
     file.map((v, i) => {
       let name = new Date().getTime() + i;
-      v.mv(`${__dirname}/public/avatar/${name}`, err => {
+      v.mv(`${__dirname}/public/avatar/${name}`, (err) => {
         if (err) {
           console.error(err);
           return res.status(500).send("err", err);
@@ -24,10 +24,10 @@ router.post("/add_user", (req, res) => {
 
   user
     .save()
-    .then(data => {
+    .then((data) => {
       res.json(data);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log("err", err);
       res.status(500).send("err", err);
     });
@@ -40,7 +40,7 @@ router.post("/update_user", (req, res) => {
     const file = req.files["images[]"];
     file.map((v, i) => {
       let name = new Date().getTime() + i;
-      v.mv(`${__dirname}/public/avatar/${name}`, err => {
+      v.mv(`${__dirname}/public/avatar/${name}`, (err) => {
         if (err) {
           console.error(err);
           return res.status(500).send("err", err);
@@ -51,15 +51,15 @@ router.post("/update_user", (req, res) => {
     });
   }
   User.findOne({ _id: req.body._id })
-    .then(data => {
+    .then((data) => {
       data.first_name = req.body.first_name;
       data.last_name = req.body.last_name;
       data.avatar = req.body.avatar;
-      data.save().then(user => {
+      data.save().then((user) => {
         res.json(user);
       });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log("err", err);
       res.status(500).send("err", err);
     });
@@ -71,33 +71,33 @@ router.get("/login", (req, res) => {
   let splitNumber = number.split(" ");
   let mergeNumber = splitNumber[0] + splitNumber[1];
   if (req.query.contact_exist) {
-      let num = ``;
+    let num = ``;
 
     let splitNumberDash = number.split("-");
-    splitNumberDash.map(v => {
+    splitNumberDash.map((v) => {
       num += v;
     });
     let splitNumberPlusZero = num.split("+0");
-    num = ``
-    splitNumberPlusZero.map(v => {
+    num = ``;
+    splitNumberPlusZero.map((v) => {
       num += v;
     });
     let splitNumberPlus = num.split("+");
 
-    num = ``
-    splitNumberPlus.map(v => {
+    num = ``;
+    splitNumberPlus.map((v) => {
       num += v;
     });
     let splitNumberPlus92 = num.split("+92");
 
-    num = ``
-    splitNumberPlus92.map(v => {
+    num = ``;
+    splitNumberPlus92.map((v) => {
       num += v;
     });
     let splitNumberSpace = num.split(" ");
 
     number = `+92`;
-    splitNumberSpace.map(v => {
+    splitNumberSpace.map((v) => {
       number += v;
     });
     var lastFive = "+92" + number.substr(number.length - 10); // => "Tabs1"
@@ -107,7 +107,7 @@ router.get("/login", (req, res) => {
   }
 
   User.findOne({ contact_no: mergeNumber }, (err, data) => {
-    if(data) console.log(data, mergeNumber, "login");
+    console.log(data, mergeNumber, "login");
     if (err) {
       console.log("err", err);
       return;
@@ -131,7 +131,6 @@ router.get("/get_user", (req, res) => {
   });
 });
 
-
 router.get("/get_all_user", (req, res) => {
   console.log(req.query, "get User");
   User.find({}, (err, data) => {
@@ -153,13 +152,38 @@ router.post("/confirm_user", (req, res) => {
     data.confirmation_code = "some_code";
     data
       .save()
-      .then(code => {
+      .then((code) => {
         res.json(code);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log("err", err);
         res.status(500).send("err", err);
       });
+  });
+});
+router.post("/if_contact_is_user", (req, res) => {
+  console.log(req.body, "get User");
+  let arr_exists = [];
+  let arr_not_exists = [];
+  req.body.contacts.map((v, i) => {
+    User.findOne({ contact_no: v.phoneNumbers }, (err, data) => {
+      if (err) {
+        console.log("err", err);
+        return;
+      }
+      if (data) {
+        let obj = { ...data, ...v }
+        arr_exists.push({...obj._doc, name: obj.name, raw_no: v.raw_no});
+      } else {
+        arr_not_exists.push(v);
+      }
+
+      let length = arr_not_exists.length + arr_exists.length;
+      if (length === req.body.contacts.length) {
+        res.json({ arr_exists, arr_not_exists });
+      }
+      // console.log(i, v.name, length, i);
+    });
   });
 });
 
